5f84ca8b343267df3c0dd82a2fe4255db3e90789
wangyaomei <mj1310514@163.com>
2018-08-08 22:46:32 +0800

    Revert "Initial commit"
    This reverts commit 7e1b71d3d06579761f49708fd28de3c0ddbc51bc.

--- src/main/java/mybatis_demo/dao/UserMapper.java
+++
@@ -1,7 +0,0 @@
-package mybatis_demo.dao;
-
-import mybatis_demo.entity.User;
-
-public interface UserMapper {
-	User selectUserByID(int id);
-}
--- src/main/java/mybatis_demo/dao/UserMapper2.java
+++
@@ -1,7 +0,0 @@
-package mybatis_demo.dao;
-
-import mybatis_demo.entity.User;
-
-public interface UserMapper2 {
-	User selectUserByID(int id);
-}
